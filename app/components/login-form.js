0ab00fe448c371638bd57d136340b79a997714e8
DevMai90 <dmai90@yahoo.com>
2021-03-15 21:02:40 -0700

    Update LoginForm component

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,12 +1,30 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
+  @tracked
+  userId = null;
+
+  get isDisabled() {
+    return !this.userId;
+  }
+  
   loginAsUserWithId(val) {
     console.log('UserId: ', val);
   }
+
   /**
-   * @param {Event & {target: HTMLFormElement}} evt
+   * 
+   * @param {Event & { target: HTMLSelectElement }} evt
+   */
+  @action
+  onSelectChanged(evt) {
+    this.userId = evt.target.value;
+  }
+
+  /**
+   * @param {Event & { target: HTMLFormElement }} evt
    */
   // We must use action so that the method is bound to the component's this context
   @action // decorator
