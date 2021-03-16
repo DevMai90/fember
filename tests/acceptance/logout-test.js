8297098011b23949d7a4b812b916f92a00c7653c
DevMai90 <dmai90@yahoo.com>
2021-03-16 12:03:15 -0700

    Stub ember service in test

--- tests/acceptance/logout-test.js
+++ tests/acceptance/logout-test.js
@@ -1,9 +1,15 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import MockAuthService from 'shlack/tests/stubs/auth-service';
 
 module('Acceptance | logging out', function(hooks) {
   setupApplicationTest(hooks);
+
+  hooks.beforeEach(function() {
+    // Look at this app. Register service:auth with the constructor (MockAuthService).
+    this.owner.register('service:auth', MockAuthService);
+  })
 
   test('visiting /teams and clicking "Logout"', async function(assert) {
     await visit('/teams');
