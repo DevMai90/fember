64754bd679019a4c622f5846ce00fd3be9a2bcdc
DevMai90 <dmai90@yahoo.com>
2021-03-16 21:26:50 -0700

    createMessage action

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -2,10 +2,17 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import fetch from 'fetch';
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 
 export default class ChatContainerComponent extends Component {
   @tracked
   messages = [];
+
+  /**
+   * @type {AuthService}
+   */
+  @service auth;
 
   @action
   async loadMessages() {
@@ -23,5 +30,25 @@ export default class ChatContainerComponent extends Component {
 
     // Don't push. Must make an assignment. Tracking system needs to see that a change has been made.
     this.messages = await response.json();
+  }
+
+  @action
+  async createMessage(body) {
+    const { channel: { id: channelId, teamId } } = this.args;
+
+    const userId = this.auth.currentUserId;
+
+    const response = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        teamId: 'linkedin',
+        channelId,
+        userId: 1,
+        body
+      })
+    });
   }
 }
